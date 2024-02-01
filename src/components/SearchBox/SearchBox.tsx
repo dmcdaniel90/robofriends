4fa2b944b28e7648c4f537368f954dc326ccd158
dmcdaniel90 <dmcdaniel9@gmail.com>
2024-02-01 12:45:12 +0000

    Installed typescript

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import {ChangeEventHandler} from 'react';
 
-const SearchBox = ({ searchChange }) => {
+type SearchBoxProps = {
+  searchChange: ChangeEventHandler<HTMLInputElement>
+}
+
+const SearchBox = ({ searchChange }: SearchBoxProps) => {
   return (
     <div className="pa2" data-testid="search-box">
       <label htmlFor="search" hidden>Search Robots</label>
